5dc61310acd887e911cc6a16ae60337b42f7ee21
Helen Lord <helenlord27@gmail.com>
2018-01-04 17:22:09 -0800

    added brand name to cos similarity

--- item_cos_sim.py
+++ item_cos_sim.py
@@ -16,19 +16,28 @@ def process_pricing_data(filename, csv_out_path):
     '''
     # Process Data
     MercariFeatureEngineering(
-        filename, '\t').engineer_features(csv_out_path)
+        filename, ',').engineer_features(csv_out_path)
     pricing_contents = pd.read_csv(csv_out_path)
     # Tfidf on lemmatized item descriptions
     vectorizer = TfidfVectorizer(
         preprocessor=lambda x: x, tokenizer=lambda x: x).fit(pricing_contents['lemmed_tokens'])
     lem_vectorized = vectorizer.transform(pricing_contents['lemmed_tokens'])
 
+    # Tfidf on brand names
+    vectorizer = TfidfVectorizer().fit(pricing_contents['brand_name'])
+    brand_vectorized = vectorizer.transform(pricing_contents['brand_name'])
+
+    # Put into one matrix
+    pricing_as_mat = sparse.hstack(
+        (lem_vectorized, brand_vectorized))
+
     # Put into similarity matrix
-    pricing_as_mat = cosine_similarity(lem_vectorized)
+    pricing_as_mat = cosine_similarity(pricing_as_mat)
+    print 'Cosine Similarity Done!'
     return pricing_contents, pricing_as_mat
 
 
-def x_most_similar(mat, item_idx, x=100, sim_thresh=0.98):
+def x_most_similar(mat, item_idx, x=100, sim_thresh=0.65):
     '''
     INPUT:
         - matrix of cosine similarities
@@ -37,11 +46,8 @@ def x_most_similar(mat, item_idx, x=100, sim_thresh=0.98):
     OUTPUT:
         - list of indices of x most similar rows
     '''
-    # sim_sort = np.argsort(mat[item_id, :50])
-    # print sim_sort
-    mask = mat[item_idx, :] > sim_thresh
-    top_idxs = np.ma.nonzero(mask)
-    top_idxs = [x for idx in top_idxs for x in idx if x != item_idx]
+    top_idxs = np.argpartition(mat, -x)[item_idx, -x:]
+    top_idxs = [idx for idx in top_idxs if idx != item_idx]
     return top_idxs
 
 
@@ -59,13 +65,23 @@ def price_diff(price_mat, idx, avg_price):
 
 if __name__ == "__main__":
     pricing_data_contents, pricing_mat = process_pricing_data(
-        '/Users/hslord/kaggle/mercari_price_suggestion/data/train_sample.tsv',
+        '/Users/hslord/kaggle/mercari_price_suggestion/data/train_sample.csv',
         'data/new_features_added.csv')
-    pricing_data_contents.iloc[877]
+    pricing_data_contents.iloc[118]
     pricing_data_contents.head()
-    similar_1 = x_most_similar(pricing_mat, 877)
+    similar_1 = x_most_similar(pricing_mat, 118)
     avg_price_1 = avg_similar_items(pricing_data_contents, similar_1)
-    print price_diff(pricing_data_contents, 877, avg_price_1)
+    print price_diff(pricing_data_contents, 118, avg_price_1)
 
     for x in similar_1:
-        print pricing_data_contents.iloc[x]['item_description']
+        print pricing_data_contents.iloc[x][['brand_name', 'item_description']]
+
+    pricing_data_contents[pricing_data_contents['brand_name']
+                          == "Victoria's Secret"]
+    pricing_data_contents.groupby('brand_name').count()
+
+# train = pd.read_csv(
+#     '/Users/hslord/kaggle/mercari_price_suggestion/data/train_sample.tsv', delimiter='\t')
+# train = train.iloc[:30000]
+# train.to_csv(
+#     '/Users/hslord/kaggle/mercari_price_suggestion/data/train_sample.csv')
